8795dd9b3626a033b5af8a37abc9f5b6a0b9faed
xxy <xiaoxinyu.hzfe@bytedance.com>
2021-08-18 11:11:55 +0800

    feat: add dist

---
+++ dist/assets/index.d91aead7.js
@@ -0,0 +1,1 @@
+import{c as s,a,u as t,o as e,b as o,d as c}from"./vendor.cd5e7ea8.js";const f={class:"cropper"};c({setup:c=>(c,p)=>(e(),s("section",f,[a(t(o),{img:"https://avatars.githubusercontent.com/u/15681693?s=88&u=2097736bf929136febb8c52df09f10290930fdef&v=4"})]))}).mount("#app");
