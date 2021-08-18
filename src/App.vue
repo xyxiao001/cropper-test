4d1966283583532abb5e74cf7efb3582865fe1d7
xxy <xiaoxinyu.hzfe@bytedance.com>
2021-08-18 11:10:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="cropper">
+    <VueCropper img="https://avatars.githubusercontent.com/u/15681693?s=88&u=2097736bf929136febb8c52df09f10290930fdef&v=4"></VueCropper>
+  </section>
+</template>
+
+<script setup>
+import 'vue-cropper/dist/index.css'
+import { VueCropper }  from "vue-cropper";
+
+</script>
+
+<style>
+.cropper {
+  width: 500px;
+  height: 500px;
+}
+</style>
