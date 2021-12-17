26ee9a1dde34d727fb9e2c53cad2ca6f61019142
xxy <xiaoxinyu.hzfe@bytedance.com>
2021-12-17 14:51:40 +0800

    feat: 修改 vue3

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <section class="cropper">
-    <VueCropper img="https://avatars.githubusercontent.com/u/15681693?s=88&u=2097736bf929136febb8c52df09f10290930fdef&v=4"></VueCropper>
+    <VueCropper ref="cropper" img="https://avatars.githubusercontent.com/u/15681693?s=88&u=2097736bf929136febb8c52df09f10290930fdef&v=4"></VueCropper>
+    <button @click="getData">获取截图</button>
   </section>
 </template>
 
 <script setup>
 import 'vue-cropper/dist/index.css'
 import { VueCropper }  from "vue-cropper";
+import { onMounted, ref } from '@vue/runtime-core';
+
+const cropper = ref(null)
+onMounted(() => {
+  console.log(cropper, 'ref');
+})
+
+const getData = () => {
+  cropper.value.getCropData(data => {
+    console.log(data, '--');
+  })
+}
 
 </script>
 
