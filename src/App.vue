879dfb7af9ce0bbabe24a59391454d9d6239a31a
xyxiao001 <463618622@qq.com>
2022-07-18 16:19:08 +0800

    fix: 更新 vite

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <section class="cropper">
-    <VueCropper ref="cropper" img="https://avatars.githubusercontent.com/u/15681693?s=88&u=2097736bf929136febb8c52df09f10290930fdef&v=4"></VueCropper>
+    <VueCropper
+      ref="cropper" 
+      auto-crop
+      mode="cover"
+      img="https://avatars.githubusercontent.com/u/15681693?s=88&u=2097736bf929136febb8c52df09f10290930fdef&v=1"
+    >
+    </VueCropper>
     <button @click="getData">获取截图</button>
   </section>
 </template>
@@ -8,16 +14,15 @@
 <script setup>
 import 'vue-cropper/dist/index.css'
 import { VueCropper }  from "vue-cropper";
-import { onMounted, ref } from '@vue/runtime-core';
+import { ref } from '@vue/runtime-core';
 
 const cropper = ref(null)
-onMounted(() => {
-  console.log(cropper, 'ref');
-})
 
 const getData = () => {
   cropper.value.getCropData(data => {
-    console.log(data, '--');
+    const img = document.createElement('img')
+    img.src = data;
+    document.body.append(img)
   })
 }
 
